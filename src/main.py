078dc5d58071cd8bfccfefae1585218bd5d55ce2
Sjors Smits <smits.sjors@gmail.com>
2019-11-13 19:14:51 +0100

    Added main and mainwidow

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from mainwindow import Window
+
+if __name__ == '__main__':
+    app = QApplication([])
+    # app.setApplicationName('skuu')
+    w = Window()
+    sys.exit(app.exec_())
