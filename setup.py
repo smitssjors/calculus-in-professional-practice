3da426eac172122de88c7aee74588e02d0b9b926
Sjors Smits <smits.sjors@gmail.com>
2019-11-18 16:03:24 +0100

    Made file structure better

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='cpp', packages=find_packages())
+setup(
+    name='cpp',
+    packages=find_packages(),
+    extras_require={
+        'dev': ['PyQt5==5.13.2', 'numpy==1.17.4', 'matplotlib==3.1.1', 'graphviz==0.13.2'],
+        'test': ['pytest==5.2.2']
+    })
